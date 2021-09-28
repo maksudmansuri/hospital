babf50d1556060cd983bb423f33eb1eea8f5934a
maksudmansuri <uniqueupgrade@gmail.com>
2021-09-28 17:41:52 +0530

    change after error and deleted notification

--- relief/asgi.py
+++ relief/asgi.py
@@ -1,27 +1,15 @@
 """
 ASGI config for relief project.
-
 It exposes the ASGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'relief.settings')
 
-from channels.auth import AuthMiddlewareStack
-from notificationapp.routing import websocket_urlpatterns
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    )
-})
+application = get_asgi_application()
+

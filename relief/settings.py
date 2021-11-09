eb0a56694cfe34f29c83f826405ac76758ea649c
maksudmansuri <uniqueupgrade@gmail.com>
2021-11-09 13:18:34 +0530

    socket issue 6

--- relief/settings.py
+++ relief/settings.py
@@ -122,7 +122,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("http://reliefproapp.herokuapp.com/", 6379)],
         },
     },
 }
